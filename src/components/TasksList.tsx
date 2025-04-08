8672b8e81c97ac2f40d39a921ad9d9e49e85338d
Bobcho101 <bgbobi878@gmail.com>
2025-04-08 14:08:51 +0300

    made the tasks list in a state

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -1,17 +1,28 @@
 import { Component } from "react";
 
-export default class TasksList extends Component {
-  render() {
-    return (
-        <ul className="space-y-3">
-            <li className="flex items-center justify-between bg-gray-700 p-3 rounded-xl shadow-sm">
-                <div className="flex items-center gap-3">
-                <input type="checkbox" className="w-4 h-4 text-blue-500 bg-gray-800 border-gray-600" />
-                <span className="text-gray-100">Sample Task</span>
-                </div>
-                <button className="text-red-400 hover:text-red-600 cursor-pointer">Delete</button>
-            </li>
-        </ul>
-    );
+interface StateI {
+    tasks: string[];
   }
+  
+export default class TasksList extends Component<unknown, StateI> {
+    constructor(props: unknown){
+        super(props);
+        this.state = {
+            tasks: ["Clean the house", "Feed the dog", "Feed the cat", "Go to the store"]
+        }
+    }
+    render() {
+        return (
+            <ul className="space-y-3">
+                {this.state.tasks.map((task, index) => <li key={index} className="flex items-center justify-between bg-gray-700 p-3 rounded-xl shadow-sm">
+                    <div className="flex items-center gap-3">
+                        <input type="checkbox" className="w-4 h-4 text-blue-500 bg-gray-800 border-gray-600" />
+                        <span className="text-gray-100">{task}</span>
+                    </div>
+                    <button className="text-red-400 hover:text-red-600 cursor-pointer">Delete</button>
+                </li>
+                )}
+            </ul>
+        );
+    } 
 }
